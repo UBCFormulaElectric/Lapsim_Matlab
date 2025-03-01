6d84adc7e1a8683489579ea313cb223c724dda27
aborawake <armanborawake@gmail.com>
2025-03-01 12:02:31 -0800

    Mac Compatibility for Model_DashBoard
    Added code to model_Dashboard_App.mlapp that allows for files to be accessed and saved when running on mac. Note please download the following add-on (https://www.mathworks.com/matlabcentral/fileexchange/59695-detectos), for the code to work.

--- runEndurance.m
+++ runEndurance.m
@@ -1,28 +1,30 @@
 function [SectorDataC_EN, ForceDataC_EN, TotalT_EN, LapLength_EN, EnergyUsed_Lap, EnergyUsed_EN, ENScalar, CP, AP] = runEndurance(CP, AP, CourseData)
 
-        [SectorDataC_EN, ForceDataC_EN, TotalT_EN, LapLength_EN, EnergyUsed_Lap] = LapModel(CP,AP,CourseData);
+    [SectorDataC_EN, ForceDataC_EN, TotalT_EN, LapLength_EN, EnergyUsed_Lap] = LapModel(CP,AP,CourseData);
             
-            EnergyAvailable = CP.AvailableEnergy;
-            ENScalar = 22000/LapLength_EN;
+    EnergyAvailable = CP.AvailableEnergy;
+    ENScalar = 22000/LapLength_EN;
            
             
-            %EnergyUsed_Lap = (sum(app.SectorDataC_EN(:,7)) + sum(app.SectorDataC_EN(:,8)))/(1000*3600);
-            EnergyUsed_EN = EnergyUsed_Lap*ENScalar;
+    %EnergyUsed_Lap = (sum(app.SectorDataC_EN(:,7)) + sum(app.SectorDataC_EN(:,8)))/(1000*3600);
+    EnergyUsed_EN = EnergyUsed_Lap*ENScalar;
             
-            InitialPmax = CP.Pmax;
-            PowerScaleFactor = EnergyAvailable/EnergyUsed_EN;
+   %%InitialPmax = CP.Pmax;
+    PowerScaleFactor = EnergyAvailable/EnergyUsed_EN;
             
-            while (EnergyUsed_EN < EnergyAvailable*0.98 || EnergyUsed_EN > EnergyAvailable*1.01) && PowerScaleFactor<=1
-            
+    while (EnergyUsed_EN < EnergyAvailable*0.98 || EnergyUsed_EN > EnergyAvailable*1.01) && PowerScaleFactor<=1
         
-                PowerScaleFactor = EnergyAvailable/EnergyUsed_EN;
+        PowerScaleFactor = EnergyAvailable/EnergyUsed_EN;
                 
-                CP.Pmax = CP.Pmax*PowerScaleFactor;
+        CP.Pmax = CP.Pmax*PowerScaleFactor;
                 
-                [SectorDataC_EN, ForceDataC_EN, TotalT_EN, LapLength_EN , EnergyUsed_Lap] = LapModel(CP,AP,CourseData);
+        [SectorDataC_EN, ForceDataC_EN, TotalT_EN, LapLength_EN , EnergyUsed_Lap] = LapModel(CP,AP,CourseData);
                 
-                %EnergyUsed_Lap = (sum(app.SectorDataC_EN(:,7)) + sum(app.SectorDataC_EN(:,8)))/(1000*3600);
-                EnergyUsed_EN = EnergyUsed_Lap*ENScalar;
-            end
+        %EnergyUsed_Lap = (sum(app.SectorDataC_EN(:,7)) + sum(app.SectorDataC_EN(:,8)))/(1000*3600);
+        EnergyUsed_EN = EnergyUsed_Lap*ENScalar;
+
+    end
+
+
 
 end
